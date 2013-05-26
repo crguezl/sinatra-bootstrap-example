63c819dfcf18dfe3358f72f8700d38c695d4ed9b
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-05-26 19:56:47 +0100

    using haml again

--- app/example-app.rb
+++ app/example-app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
-#require 'haml'
-require 'erb'
+require 'haml'
+#require 'erb'
 
 module Example
   class App < Sinatra::Base
@@ -9,14 +9,14 @@ module Example
     set :public_folder, "#{current_dir}/../public/"
 
     get '/' do
-      #haml :index, :locals => { :name => 'World!' }
       puts current_dir
-      erb :index, :locals => { :name => 'World!' }
+      haml :index, :locals => { :name => 'World!' }
+      #erb :index, :locals => { :name => 'World!' }
     end
 
     get '/:name' do |name|
-      #haml :index, :locals => { :name => name }
-      erb :index, :locals => { :name => name }
+      haml :index, :locals => { :name => name }
+      #erb :index, :locals => { :name => name }
     end
   end
 end
