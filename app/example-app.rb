958e9e1ff85603cb178808d63648378f24772d8f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-05-26 19:53:56 +0100

    can not find static files. Explicitly setting the public directory

--- app/example-app.rb
+++ app/example-app.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
-require 'haml'
+#require 'haml'
+require 'erb'
 
 module Example
   class App < Sinatra::Base
 
+    current_dir = File.dirname(__FILE__)
+    set :public_folder, "#{current_dir}/../public/"
+
     get '/' do
-      haml :index, :locals => { :name => 'World!' }
+      #haml :index, :locals => { :name => 'World!' }
+      puts current_dir
+      erb :index, :locals => { :name => 'World!' }
     end
 
     get '/:name' do |name|
-      haml :index, :locals => { :name => name }
+      #haml :index, :locals => { :name => name }
+      erb :index, :locals => { :name => name }
     end
   end
 end
